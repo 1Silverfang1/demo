722ba021a1b138b5df88c1164769bd2ec2bb9fab
root <root@localhost.localdomain>
2020-01-14 16:04:45 +0530

    final

--- src/main/java/com/redbus/backend_redbus/controller/HomeController.java
+++ src/main/java/com/redbus/backend_redbus/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.redbus.backend_redbus.model.*;
 import com.redbus.backend_redbus.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 //    @Autowired
 //    private BusTicketRepository busTicketRepository;
